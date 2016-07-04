028bea46e024237fe5f36d92f7642573b901010e
Vincent MONTECOT <vmonteco@e2r10p13.42.fr>
2016-07-04 11:33:48 +0200

    troll script added

---
+++ nfs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from time import sleep
+import webbrowser
+
+if __name__ == '__main__':
+    while True:
+        print("Come on! Locking your session isn't that hard!")
+        webbrowser.open_new('https://www.youtube.com/watch?v=dQw4w9WgXcQ') 
+        webbrowser.open_new('https://cdn.meme.am/instances/500x/69235525.jpg')
+        sleep(10)
